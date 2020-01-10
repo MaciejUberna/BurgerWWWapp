6adff9bd153103e3691ec1e932dfac914d5e8e10
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-10 20:11:19 +0100

    Lesson_155_First-BurgerBuilderTemplate_added

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Layout from './coponents-stateLess/Layout/Layout';
+import BurgerBuilder from './containers-stateFul/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
     <div>
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder/>
       </Layout>
     </div>
   );}
