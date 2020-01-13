06bcb0772327fb5da5f7ce919160fc6fcb944924
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-13 20:38:20 +0100

    Lesson160 Array with ingredients is flattened with reduce() array function so if all ingredients are 0 it returns an empty array (one level only).

--- burger-app/src/coponents-stateLess/Burger/Burger.js
+++ burger-app/src/coponents-stateLess/Burger/Burger.js
@@ -4,12 +4,22 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
  
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
         return [...Array(props.ingredients[ingredientKey])].map((_,i) => {
             return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>
         });
-    });
+    })
+    .reduce((array,element) => {
+        return array.concat(element);
+    }, []);
+
+    //console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Zacznij wprowadzać składniki.</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
