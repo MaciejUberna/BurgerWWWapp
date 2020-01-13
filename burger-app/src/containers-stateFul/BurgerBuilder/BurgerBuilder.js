ef7aecb1029084b1bd4c5043f9976ed09f91f74a
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-13 18:43:50 +0100

    Lesson158 burger added to BurgerBuilder hardcoded right now.

--- burger-app/src/containers-stateFul/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers-stateFul/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../coponents-stateLess/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render () {
         return (
             <Auxiliary>
-                <div>Burger</div>
+                <Burger/>
                 <div>Build Controls</div>
             </Auxiliary>
         );
