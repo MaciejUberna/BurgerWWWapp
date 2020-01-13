4fd5d981156f86c72a0506d6c522e2f6ab139bd0
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-13 21:13:51 +0100

    Lesson161 BuildControls and BuildControl components added with css module styling. Not connected yet.

---
+++ burger-app/src/coponents-stateLess/Burger/BuildControls/BuildControl/BuildComntrol.js
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+import classes from './BuildControl.module.css'
+ 
+const buildControl = (props) => {
+    return (
+        <div className={classes.BuildControl}>
+            <div className={classes.Label}>{props.ingredientLabel}</div>
+            <button className={classes.Less}>Odejmij</button>
+            <button className={classes..More}>Dodaj</button>
+        </div>
+    );
+}
+ 
+export default buildControl;
