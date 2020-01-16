88402af858915ea7940e7432c5c5dea3f11a3ec0
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-16 21:14:38 +0100

    Lesson173 "Adding a Toolbar" connectiong a toolbar in Layout and fixing a Layout style so that toolbar does not overlap burger.

--- burger-app/src/coponents-stateLess/Layout/Layout.js
+++ burger-app/src/coponents-stateLess/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Auxiliary>
         <div>
-            Toolbar, SideDrawer, Backdrop
+            <Toolbar/>
         </div>
         <main className={classes.Content}>
             {props.children}
