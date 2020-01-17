fe845f276b020b3c794a27ce038b3e97071fe775
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-17 18:43:11 +0100

    Lesson175 "Adding Reusable Navigation Items"  - connected NavigationItems.js to Toolbar.js.

--- burger-app/src/coponents-stateLess/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger-app/src/coponents-stateLess/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -5,9 +5,9 @@ const navigationItem = (props) => {
     return (
         <li className={classes.NavigationItem}>
             <a
-                classNamne={props.active ? classes.active : null}
+                className={props.active ? classes.active : null}
                 href={props.link} 
-                className={}>
+            >
                 {props.children}
             </a>
         </li>
