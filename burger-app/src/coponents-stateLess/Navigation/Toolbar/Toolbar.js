929d4e55a9317813eba3196b5c87f36d1ad469f5
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-17 19:54:50 +0100

    Lesson177 "Working on Responsive Adjustments" first approach: Dynamic logo adjustment by passing height parameter in Logo object.

--- burger-app/src/coponents-stateLess/Navigation/Toolbar/Toolbar.js
+++ burger-app/src/coponents-stateLess/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,7 @@ const toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
             <div>MENU</div>
-            <Logo/>
+            <Logo height="100%"/>
             <nav>
                 <NavigationItems/>
             </nav>
