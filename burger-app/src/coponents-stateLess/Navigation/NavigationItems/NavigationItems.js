5a44480c0181d97686147d0ea88bef2d2085b755
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-17 18:07:31 +0100

    Lesson174 "Adding Reusable Navigation Items" added NavigationItem.js to NavigationItems.js

--- burger-app/src/coponents-stateLess/Navigation/NavigationItems/NavigationItems.js
+++ burger-app/src/coponents-stateLess/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,9 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => {
     return (
         <ul className={classes.NavigationItems}>
-            <NavigationItem />
+            {/* For bulien variables we just have to write variable name without value so it's true */}
+            <NavigationItem link="/" active>Burger Builder</NavigationItem>
+            <NavigationItem link="/" >Checkout</NavigationItem>
         </ul>
     );
 }
