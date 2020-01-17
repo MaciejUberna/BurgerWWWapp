7ffe7eb104f341cf345c2229e5adae429de415a4
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-17 18:20:23 +0100

    Lesson175 - added ALT parameter to Logo.js

--- burger-app/src/coponents-stateLess/Logo/Logo.js
+++ burger-app/src/coponents-stateLess/Logo/Logo.js
@@ -6,7 +6,7 @@ import burgerLogo from '../../assets/images/burger-logo.png'
 const logo = (props) => {
     return (
         <div className={classes.Logo}>
-            <img src={burgerLogo} />
+            <img src={burgerLogo} alt="Logo"/>
         </div>
     );
 }
