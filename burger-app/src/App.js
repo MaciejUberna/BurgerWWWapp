cc85f2064990db50814b651231051685d70c3a6c
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-19 21:10:36 +0100

    Lesson186 "Changing the Folder Structure" - App.js modified so it fings Layout.js component correctlly in hoc.

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Layout from './coponents-stateLess/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers-stateFul/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
