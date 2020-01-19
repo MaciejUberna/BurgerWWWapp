ef089dbb095d0e0a977c9ed5a40a3680680a05cf
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-19 20:53:27 +0100

    Lesson184 "Improving Performance" - added PropTypes to OrderSummary and changed Modal into class from func. . Now modal uses life-cycle-hook shouldComponentUpdate if show is different then in current state

--- burger-app/src/coponents-stateLess/Burger/OrderSummary/OrderSummary.js
+++ burger-app/src/coponents-stateLess/Burger/OrderSummary/OrderSummary.js
@@ -1,7 +1,7 @@
-import React from 'react';
- 
+import React, { Component } from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
+import PropTypes from 'prop-types';
 
 const INGREDIENTS_TO_POLISH = {
     meat: 'mięsko',
@@ -10,34 +10,49 @@ const INGREDIENTS_TO_POLISH = {
     salad: 'sałata'
 }
 
-const orderSummary = (props) => {
-    const ingridientSummary = Object.keys(props.ingredients)
-        .map(ingredientKey => {
-            //Outer curly baraces in span are for marking a dynamic entry and inner are just javascript object
-            return ( 
-            <li key={ingredientKey} >
-                <span style={{textTransform: "capitalize"}}>
-                    {INGREDIENTS_TO_POLISH[ingredientKey]}
-                </span> 
-                : {props.ingredients[ingredientKey]}
-            </li>
-            )
-        });
-    
-    return(
-        <Auxiliary>
-            <h3>Twoje Zamówienie</h3>
-            <p>Smaczny hamburger z super składnikami: </p>
-            <ul>
-                {ingridientSummary}
-            </ul>
-            {/* <p>Continue checkout?</p> */}
-            <p><strong>Ostateczna cena: {props.price.toFixed(2)} zł.</strong></p>
-            <p>Przejść Dalej?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>ANULUJ</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>DALEJ</Button>
-        </Auxiliary>
-    );
+class OrderSummary  extends Component {
+
+    //This could be a functional component, doesn't nave to be a class
+    componentDidUpdate() {
+        console.log('[OrderSummary] did update');
+    }
+
+    render() {
+        const ingridientSummary = Object.keys(this.props.ingredients)
+            .map(ingredientKey => {
+                //Outer curly baraces in span are for marking a dynamic entry and inner are just javascript object
+                return ( 
+                <li key={ingredientKey} >
+                    <span style={{textTransform: "capitalize"}}>
+                        {INGREDIENTS_TO_POLISH[ingredientKey]}
+                    </span> 
+                    : {this.props.ingredients[ingredientKey]}
+                </li>
+                )
+            });
+        
+        return(
+            <Auxiliary>
+                <h3>Twoje Zamówienie</h3>
+                <p>Smaczny hamburger z super składnikami: </p>
+                <ul>
+                    {ingridientSummary}
+                </ul>
+                {/* <p>Continue checkout?</p> */}
+                <p><strong>Ostateczna cena: {this.props.price.toFixed(2)} zł.</strong></p>
+                <p>Przejść Dalej?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>ANULUJ</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>DALEJ</Button>
+            </Auxiliary>
+        );
+    }
+}
+
+OrderSummary.propTypes = {
+    ingredients: PropTypes.object,
+    price: PropTypes.number.isRequired,
+    purchaseCancelled: PropTypes.func.isRequired,
+    purchaseContinued: PropTypes.func.isRequired
 }
  
-export default orderSummary;
+export default OrderSummary;
--- burger-app/src/coponents-stateLess/UI/Modal/Modal.js
+++ burger-app/src/coponents-stateLess/UI/Modal/Modal.js
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classes from './Modal.module.css'
 
 import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
- 
-const modal = (props) => { 
-    return (
-        <Auxiliary>
-            <Backdrop show={props.show} clicked={props.modalClosed}/>
-            <div className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-            >
-                {props.children}
-            </div>
-        </Auxiliary>
-    );
+
+//Alternative i reactMemo wrap
+class Modal extends Component { 
+
+    //We are not using PureComponent because it will do too many checks
+    shouldComponentUpdate(nextProps,nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentDidUpdate() {
+        console.log('[Modal] updated.')
+    }
+
+    render() {
+        return (
+            <Auxiliary>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}
+                >
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        );
+    }
 }
 
-modal.propTypes = {
+Modal.propTypes = {
     children: PropTypes.object.isRequired,
     show: PropTypes.bool.isRequired,
     clicked: PropTypes.func
 };
  
-export default modal;
+export default Modal;
