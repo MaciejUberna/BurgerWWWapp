f9de47681d6cd61d6a57663fbffade53947e12ce
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-19 17:23:53 +0100

    Lesson181 "Adding a Hamburger Icon" - there are 3 lines for the menu (in DrawerToggle.js - menu ) not hamburger icon the was added way back.

--- burger-app/src/coponents-stateLess/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ burger-app/src/coponents-stateLess/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -5,7 +5,9 @@ import classes from './DrawerToggle.module.css'
 const menu = (props) => {
     return (
         <div className={classes.DrawerToggle} onClick={props.clicked}>
-            MENU
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     );
 }
