65dec33d9fae1114e8d99349db6d8648e9125d1b
Maciej Uberna <maciej.uberna@gmail.com>
2020-03-02 00:29:41 +0100

    Lesson257 "Order Submission & Passing Data Between Pages" created orderHandler with dummy user data that processes the order and sends it to firebase (MaciejUberna) account.

--- burger-app/src/containers-stateFul/Checkout/ContactData/ContactData.js
+++ burger-app/src/containers-stateFul/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Button from '../../../coponents-stateLess/UI/Button/Button';
+import Spinner from '../../../coponents-stateLess/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
     state = {
@@ -10,21 +12,61 @@ class ContactData extends Component {
             street: '',
             postalCode: '',
             city: ''
-        }
+        },
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        //console.log('ingredients in ContactData: ',this.props.ingredients);
+
+        this.setState({loading: true});
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Maciej',
+                address: {
+                    street: 'InfineteLoopStreet 1',
+                    postalCode: '99-999',
+                    city: 'Warsaw'
+                },
+                email: 'test1@test.com'
+            },
+            deliveryMethod: 'fastests'
+        };
+
+        //for firebase its .json node
+        axios.post('/orders.json',order)
+            .then(response => {
+                this.setState({loading: false});
+                this.props.history.push('/');
+                console.log('Response to order: ',response)
+            })
+            .catch(error => {
+                this.setState({loading: false});
+                console.log('Response to order error: ',error)
+            });
     }
 
     render () {
+        let form = (
+            <form>
+                <input className={classes.Input} type='text' name='name' placeholder='Twoje Imie' />
+                <input className={classes.Input} type='email' name='email' placeholder='Twój email' />
+                <input className={classes.Input} type='text' name='street' placeholder='Twoja ulica' />
+                <input className={classes.Input} type='text' name='postalCode' placeholder='kod-pocztowy' />
+                <input className={classes.Input} type='text' name='city' placeholder='miasto' />
+                <Button btnType='Success' clicked={this.orderHandler}>Zamówienie</Button>
+            </form>
+        );
+        if(this.state.loading) {
+            form = <Spinner />;
+        }
         return (
             <div className={classes.ContactData}>
                 <h4>Wprowadź swoje dane kontaktowe</h4>
-                <form>
-                    <input className={classes.Input} type='text' name='name' placeholder='Twoje Imie' />
-                    <input className={classes.Input} type='email' name='email' placeholder='Twój email' />
-                    <input className={classes.Input} type='text' name='ulica' placeholder='Twoja ulica' />
-                    <input className={classes.Input} type='text' name='kod-pocztowy' placeholder='kod-pocztowy' />
-                    <input className={classes.Input} type='text' name='miejscowość' placeholder='miejscowość' />
-                    <Button btnType='Success'>Zamówienie</Button>
-                </form>
+                {form}
             </div>
         );
     }
