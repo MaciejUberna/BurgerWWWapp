bb9e40739835572f6706fc49c9d50608fb1e1d4d
Maciej Uberna <maciej.uberna@gmail.com>
2020-03-03 17:20:44 +0100

    Lesson260 "Fetching Orders" now only saving them in orders state.

--- burger-app/src/containers-stateFul/Orders/Orders.js
+++ burger-app/src/containers-stateFul/Orders/Orders.js
@@ -1,8 +1,37 @@
 import React , { Component } from 'react';
-
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import Order from '../../coponents-stateLess/Order/Order';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const orders = [];
+                for(let key in res.data) {
+                    orders.unshift({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                console.log('Orders, componentDidMount, response: ',res);
+                this.setState({
+                    loading: false,
+                    orders: orders
+                });
+            })
+            .catch(err => {
+                this.setState({loading: false});
+                console.log('Orders, componentDidMount, error: ',err);
+            })
+    }
+
     render () {
         return (
             <div>
@@ -13,4 +42,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders,axios);
