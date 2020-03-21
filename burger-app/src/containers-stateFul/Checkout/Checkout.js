c1d3c7995e28ced92386993ac60f548b3b1778df
Maciej Uberna <maciej.uberna@gmail.com>
2020-03-21 15:50:22 +0100

    Lesson310 "Adjusting Checkout and Contact Data" adjusted Checkout.js with connect react-redux and removed local sate and componentDidMount. This also includes <ContactData> mod so it no longer renders and does not passes ingredients and price so thet Redux can do the job.

--- burger-app/src/containers-stateFul/Checkout/Checkout.js
+++ burger-app/src/containers-stateFul/Checkout/Checkout.js
@@ -1,46 +1,10 @@
 import React, {Component} from 'react';
-import queryString from 'query-string';
 import CheckoutSummary from '../../coponents-stateLess/Order/CheckoutSummary/CheckoutSummary';
 import {Route} from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
+import { connect } from 'react-redux';
 
 class Checkout extends Component {
-
-    state = {
-        ingredients: {},
-        price: 0
-    }
-
-    //Maciek's way
-    componentDidMount() {
-        if(this.props.location.search) {
-            const values = queryString.parse(this.props.location.search, {parseNumbers: true});
-            const ingredients = {};
-            let totalPrice = 0;
-            for (let item in values) {
-                if(item === 'price')
-                    totalPrice = values[item];
-                else
-                    ingredients[item] = values[item];
-            }
-            this.setState({
-                ingredients: ingredients,
-                price: totalPrice
-            });
-        } else {
-            console.log('Checkout.js didMount: no search history present.')
-        }
-    }
-
-    //Max way
-    // componentDidMount() {
-    //     const query = new URLSearchParams(this.props.location.search);
-    //     const ingredients = {};
-    //     for(let param of query.entries()) {
-    //         ingredients[param[0]] = +param[1];
-    //     }
-    //     this.setState({ingredients: ingredients});
-    // }
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
@@ -54,24 +18,23 @@ class Checkout extends Component {
         //console.log('Render Checkout.js : Ingredients :: ',this.state.ingredients)
         return(
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients}
+                <CheckoutSummary ingredients={this.props.ings}
                 checkoutCancelled={this.checkoutCancelledHandler}
                 checkoutContinued={this.checkoutContinuedHandler} 
                 />
                 <Route 
                     path={this.props.match.path + '/contact-data'} 
-                    render={(props) => 
-                        (
-                        <ContactData 
-                            ingredients={this.state.ingredients} 
-                            price={this.state.price}
-                            {...props}
-                            />
-                        )} 
+                    component={ContactData}
                 />
             </div>
         );
     }
 }
 
-export default Checkout;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+    }
+}
+
+export default connect(mapStateToProps)(Checkout);
