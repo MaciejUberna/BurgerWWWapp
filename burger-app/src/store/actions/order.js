eb5b32089c76eadd695552a56dde47cc4531b1da
Maciej Uberna <maciej.uberna@gmail.com>
2020-03-26 21:53:43 +0100

    Lesson312 "Combining Reducers" fix order.js action creator in purchaseBurger so the it fetches correct id not response.data, but response.data.name .

--- burger-app/src/store/actions/order.js
+++ burger-app/src/store/actions/order.js
@@ -28,7 +28,7 @@ export const purchaseBurger = (orderData) => {
         axios.post('/orders.json',orderData)
         .then(response => {
             console.log('Response data: ',response.data);
-            dispatch(purchaseBurgerSuccess(response.data,orderData));
+            dispatch(purchaseBurgerSuccess(response.data.name,orderData));
         })
         .catch(error => {
             console.log('Response to order error: ',error);
