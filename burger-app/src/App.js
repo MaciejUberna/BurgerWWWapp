d2d4927943a4f5aaa9f8a7b7a1d86ed374336234
Maciej Uberna <maciej.uberna@gmail.com>
2020-03-29 21:31:05 +0200

    Lesson324 "Adding an Auth Form" added route in App.js pointing to Auth.js the same in NavigationItems.js with links to <NavigationItem link="/auth">Logowanie</NavigationItem>.

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers-stateFul/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers-stateFul/Checkout/Checkout';
 import Orders from './containers-stateFul/Orders/Orders';
+import Auth from './containers-stateFul/Auth/Auth';
 
 class App extends Component {
 
@@ -27,6 +28,7 @@ class App extends Component {
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
           <Route path="/" exact component={BurgerBuilder} />
         </Switch>
       </Layout>
