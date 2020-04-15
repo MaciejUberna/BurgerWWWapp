5c805559fb4d7c2dc0411bd640cca8818128262b
Maciej Uberna <maciej.uberna@gmail.com>
2020-04-15 16:07:59 +0200

    Lesson358 "Jest and Enzyme Documentation" wrote another test using Jest and Enzyme documentation.

--- burger-app/src/coponents-stateLess/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-app/src/coponents-stateLess/Navigation/NavigationItems/NavigationItems.test.js
@@ -6,6 +6,9 @@ import Adapter from 'enzyme-adapter-react-16';
 import NavigationItems from './NavigationItems';
 import NavigationItem from './NavigationItem/NavigationItem';
 
+//Jest : https://jestjs.io/en/
+//Enzyme : https://enzymejs.github.io/enzyme/
+
 configure({adapter: new Adapter()});
 
 //Dont have to import that describe, it and beforeEach, expect. It will be run automatically.
@@ -18,6 +21,7 @@ describe('<NavigationItems />', () => {
     it('should render two <NavigationItem /> elements if not authenticated.',() => {
         
         //Expect defines the thing we want to check, we apply non jsx elements in expect!
+        //toHaveLength is the jest liblary
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
 
@@ -25,5 +29,10 @@ describe('<NavigationItems />', () => {
         // wrapper = shallow(<NavigationItems isAuthenticated/>);
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render three <NavigationItem /> element with /logout link and "Wyloguj" sign if is authenticated.',() => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Wyloguj</NavigationItem>)).toEqual(true);
     });
 });
