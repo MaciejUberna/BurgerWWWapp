61a4691a1641e29846e4504beb0037f52dacc808
Maciej Uberna <maciej.uberna@gmail.com>
2020-05-09 17:51:33 +0200

    Lesson413 "Creating our First Saga" created saga file in src/store/sagas/auth.js .

---
+++ burger-app/src/store/sagas/auth.js
@@ -0,0 +1,12 @@
+import { put } from 'redux-saga/effects';
+
+import * as ActionTypes from '../actions/actionTypes';
+
+function* logout( action ) {
+    yield localStorage.removeItem('token');
+    yield localStorage.removeItem('expirationDate');
+    yield localStorage.removeItem('userId');
+    yield put({
+        type: ActionTypes.AUTH_LOGOUT
+    });
+}
