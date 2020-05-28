42736e24cff94c843dde3f6a627525196926f46d
Maciej Uberna <maciej.uberna@gmail.com>
2020-05-28 20:49:40 +0200

    Lesson461 "Adding a Custom Error Handling Hook" added http-error-haldler.js that handles withErrorHandler.js logic.

--- burger-app/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-app/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import useHttpErrorHandler from '../../hooks/http-error-handler';
 
 import Modal from '../../coponents-stateLess/UI/Modal/Modal';
 import Axiliary from '../Auxiliary/Auxiliary';
@@ -6,33 +7,12 @@ import Axiliary from '../Auxiliary/Auxiliary';
 const withComponentHandler = (WrappedComponent, axios) => {
     return props => {
 
-        const [error, setError] = useState(null);
-
-        const reqInterceptor = axios.interceptors.request.use(req => {
-            setError(null);
-            return req;
-        });
-
-        const resInterceptor = axios.interceptors.response.use(response => response, err => {
-            setError(err);
-        });
-
-        //Prevents memory licks when component is not needed anymore
-        useEffect(() => {
-            return () => {
-                axios.interceptors.request.eject(reqInterceptor);
-                axios.interceptors.response.eject(resInterceptor);
-            };
-        },[reqInterceptor,resInterceptor]);
-
-        const errorConfirmedHandler = () => {
-            setError(null);
-        };
+        const [error, clearError] = useHttpErrorHandler(axios);
 
         return (
                 <Axiliary>
                     <Modal show={error ? true : false}
-                    modalClosed={errorConfirmedHandler}
+                    modalClosed={clearError}
                     >
                         {error ? error.message : null}
                     </Modal>
---
+++ burger-app/src/hooks/http-error-handler.js
@@ -0,0 +1,31 @@
+import {useState, useEffect} from 'react';
+
+export default HttpClient => {
+    const [error, setError] = useState(null);
+
+    const reqInterceptor = HttpClient.interceptors.request.use(req => {
+        setError(null);
+        return req;
+    });
+
+    const resInterceptor = HttpClient.interceptors.response.use(response => response, err => {
+        setError(err);
+    });
+
+    const req = HttpClient.interceptors.request;
+    const res = HttpClient.interceptors.response;
+
+    //Prevents memory licks when component is not needed anymore
+    useEffect(() => {
+        return () => {
+            req.eject(reqInterceptor);
+            res.eject(resInterceptor);
+        };
+    },[res,req,reqInterceptor,resInterceptor]);
+
+    const errorConfirmedHandler = () => {
+        setError(null);
+    };
+
+    return [error, errorConfirmedHandler]
+};
