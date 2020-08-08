21effc4902013e97b55274e689ebda286ab3140b
Maciej Uberna <maciej.uberna@gmail.com>
2020-08-08 22:23:02 +0200

    snall adjustments.

--- burger-app/src/store/actions/onetimeJobs.js
+++ burger-app/src/store/actions/onetimeJobs.js
@@ -4,12 +4,12 @@ export const getDisclaimer = (state) => {
     return {
         type: actionTypes.ONETIME_JOBS_DISCLAIMER,
         ...state
-    }
-}
+    };
+};
 
 export const disclaimerResponded = (response) => {
     return {
         type: actionTypes.ONETIME_JOBS_DISCLAIMER_FILLED,
         responseToDisclaimer: response
-    }
-}
+    };
+};
