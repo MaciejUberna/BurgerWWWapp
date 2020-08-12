7215f3914f46158585399cbf1d143922d8f864c4
Maciej Uberna <maciej.uberna@gmail.com>
2020-08-12 14:57:00 +0200

    2/5 (organising orders by date and userId) in order.js sagas added action.userId to deleteOrderSaga and purchaseBurgerSaga and fetchOrdersSaga.

--- burger-app/src/store/sagas/order.js
+++ burger-app/src/store/sagas/order.js
@@ -7,7 +7,7 @@ import * as actions from '../actions';
 export function* purchaseBurgerSaga(action) {
     yield put(actions.purchaseBurgerStart());
     try {
-        const response = yield axios.post('/orders.json?auth=' + action.authToken,action.orderData);
+        const response = yield axios.post('/orders/'+ action.userId +'.json?auth=' + action.authToken, action.orderData);
         yield put(actions.purchaseBurgerSuccess(response.data.name,action.orderData));
     } catch(error) {
         yield put(actions.purchaseBurgerFail(error));
@@ -17,8 +17,8 @@ export function* purchaseBurgerSaga(action) {
 export function* deleteOrderSaga(action){
     yield put(actions.deleteOrderStart());
     try {
-        yield axios.delete('/orders/'+action.id+'.json?auth='+action.authToken);
-        //console.log('Response: ',response)
+        yield axios.delete('/orders/'+ action.userId +'/'+ action.id +'.json?auth='+action.authToken);
+        //console.log('Response: ',response);
         yield put(actions.deleteOrderSuccess(action.id));
     } catch (error){
         yield put(actions.deleteOrderFail(error));
@@ -27,9 +27,9 @@ export function* deleteOrderSaga(action){
 
 export function* fetchOrdersSaga(action) {
     yield put(actions.fetchOrdersStart());
-    const queryParams = yield '?auth=' + action.authToken + '&orderBy="userId"&equalTo="' + action.userId +'"';
+    const queryParams = yield '?auth=' + action.authToken + '&orderBy="dateOfOrder"';
     try {
-        const response = yield axios.get('/orders.json' + queryParams);
+        const response = yield axios.get('/orders/'+ action.userId +'.json' + queryParams);
         const orders = yield [];
         for(let key in response.data) {
             yield orders.unshift({
