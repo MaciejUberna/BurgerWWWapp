d4861ab7c2ce18a7cd2aca1417c81cac05f88dda
Maciej Uberna <maciej.uberna@gmail.com>
2020-08-18 18:12:04 +0200

    2/3 Created custom modal for displaying burgers in ModalAppear.js and ModalAppear.module.css .

---
+++ burger-app/src/coponents-stateLess/UI/Modal/ModalAppear.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classes from './ModalAppear.module.css'
+
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
+import Backdrop from '../Backdrop/Backdrop';
+
+const ModalAppear = props => { 
+
+    return (
+        <Auxiliary>
+            <Backdrop show={props.show} clicked={props.modalClosed}/>
+            <div className={classes.ModalAppear}
+            onClick={props.modalClosed}
+            style={{
+                opacity: props.show ? '1' : '0'
+            }}
+            >
+                {props.children}
+            </div>
+        </Auxiliary>
+    );
+};
+
+ModalAppear.propTypes = {
+    show: PropTypes.bool.isRequired,
+    clicked: PropTypes.func
+};
+ 
+export default React.memo(ModalAppear,(prevProps, nextProps) => 
+    nextProps.show === prevProps.show && 
+    nextProps.children === prevProps.children
+);
