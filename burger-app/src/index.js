458ed033cacb2171eb907156daa9e56d0d0a1495
Maciej Uberna <maciej.uberna@gmail.com>
2020-09-16 17:56:27 +0200

    Added brackets to Redux Dev-Tools so that browser without this tools will work.

--- burger-app/src/index.js
+++ burger-app/src/index.js
@@ -27,7 +27,7 @@ import onetimeJobsReducer from './store/reducers/onetimeJobs';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  : null || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  : null) || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
